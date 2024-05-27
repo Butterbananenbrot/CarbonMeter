297803f5aae85c79683463288699570023491bec
Butterbananenbrot <jh.hagedorn@gmail.com>
2024-05-27 12:44:48 +0200

    added a logo and centered the app

--- CarbonMeter/src/Header.jsx
+++ CarbonMeter/src/Header.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import Nav from './Nav';
+import thermometerIcon from '/src/assets/thermometer.svg';
 
 const Header = () => {
   return (
     <header>
-      <h1>CarbonMeter</h1>
-      <Nav />
+      <h1>
+        <img src={thermometerIcon} alt="Thermometer Icon" className="logo" />
+        CarbonMeter
+      </h1>
+      <nav>
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
     </header>
   );
 };
 
 export default Header;
+
