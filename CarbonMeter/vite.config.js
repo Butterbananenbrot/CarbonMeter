dfd039e05dee0d792db0c70e6116fb2b9641d816
Butterbananenbrot <jh.hagedorn@gmail.com>
2024-06-02 17:33:26 +0200

    attempt3

--- CarbonMeter/vite.config.js
+++ CarbonMeter/vite.config.js
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/CarbonMeter/', // Ensure this is the correct base path for your repository
   plugins: [react()],
   build: {
     outDir: 'dist',
   },
 })
+
